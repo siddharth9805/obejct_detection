27a0b0a7ba3ffed98e97f6c28ae912a7aab7b52c
siddharth9805 <siddharth.u@somaiya.edu>
2019-03-14 02:39:43 +0530

    added camera as whatsapp

--- app/src/main/java/com/sidhero/imagedetector/MainActivity.java
+++ app/src/main/java/com/sidhero/imagedetector/MainActivity.java
@@ -1,13 +1,58 @@
 package com.sidhero.imagedetector;
 
+
+import android.Manifest;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RunTimePermission runTimePermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        runTimePermission = new RunTimePermission(this);
+        runTimePermission.requestPermission(new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }, new RunTimePermission.RunTimePermissionListener() {
+
+            @Override
+            public void permissionGranted() {
+                // First we need to check availability of play services
+                startActivity(new Intent(MainActivity.this,CameraActivity.class));
+                finish();
+
+            }
+
+            @Override
+            public void permissionDenied() {
+
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(runTimePermission!=null){
+            runTimePermission.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
     }
 }
+
---
+++ app/src/main/java/com/sidhero/imagedetector/PhotoVideoRedirectActivity.java
@@ -0,0 +1,91 @@
+package com.sidhero.imagedetector;
+
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableDecoderCompat;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+/**
+ * Created by sotsys016-2 on 13/8/16 in com.cnc3camera.
+ */
+public class PhotoVideoRedirectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photovideo_redirect);
+
+
+        init();
+
+    }
+    VideoView videoView;
+    private void init() {
+
+        ImageView imgShow = (ImageView) findViewById(R.id.imgShow);
+        videoView = (VideoView) findViewById(R.id.vidShow);
+
+        if(getIntent().getStringExtra("WHO").equalsIgnoreCase("Image")){
+
+            imgShow.setVisibility(View.VISIBLE);
+
+            Glide.with(PhotoVideoRedirectActivity.this)
+                    .load(getIntent().getStringExtra("PATH"))
+                    .into(imgShow);
+        }else {
+
+            videoView.setVisibility(View.VISIBLE);
+            try {
+                videoView.setMediaController(null);
+                videoView.setVideoURI(Uri.parse(getIntent().getStringExtra("PATH")));
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            videoView.requestFocus();
+            //videoView.setZOrderOnTop(true);
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                public void onPrepared(MediaPlayer mp) {
+
+                    videoView.start();
+                }
+            });
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    videoView.start();
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (videoView.isPlaying()) {
+            videoView.pause();
+        }
+        super.onBackPressed();
+    }
+}
